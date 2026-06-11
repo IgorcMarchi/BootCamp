01a615e151b94fe68bf4afd9cc03020ed20a95cd
Igor Carvalho Marchi <igorcmarchi@gmail.com>
2026-06-11 18:21:10 -0300

    Card 12 Finalizando

--- Card12/pratica/dags/steam_pipeline.py
+++ Card12/pratica/dags/steam_pipeline.py
@@ -1,3 +1,4 @@
+# Imports 
 from airflow import DAG
 from airflow.operators.python import PythonOperator, BranchPythonOperator
 from airflow.operators.bash import BashOperator
@@ -6,8 +7,8 @@ from datetime import datetime
 import requests
 
 
-def get_games():
-    url = "https://api.steampowered.com/ISteamChartsService/GetMostPlayedGames/v1/"
+def get_games(): # Função para obter os dados dos jogos mais jogados da Steam
+    url = "https://api.steampowered.com/ISteamChartsService/GetMostPlayedGames/v1/" # API da Steam para obter os jogos mais jogados
 
     response = requests.get(url)
     data = response.json()
@@ -15,14 +16,14 @@ def get_games():
     return data["response"]["ranks"][:10]
 
 
-def save_ranking(ti):
+def save_ranking(ti): # Função para salvar o ranking dos jogos em um arquivo de texto
     games = ti.xcom_pull(task_ids="get_games")
 
-    with open("/opt/airflow/data/ranking.txt", "w", encoding="utf-8") as f:
+    with open("/opt/airflow/data/ranking.txt", "w", encoding="utf-8") as f: # Abre o arquivo para escrita, criando-o se não existir
 
         f.write("TOP 10 JOGOS DA STEAM\n\n")
 
-        for game in games:
+        for game in games: # Itera sobre os jogos e escreve suas informações no arquivo
             f.write(
                 f"Rank {game['rank']} | "
                 f"AppID {game['appid']} | "
@@ -32,7 +33,7 @@ def save_ranking(ti):
     print("Ranking salvo com sucesso!")
 
 
-def choose_popularity(ti):
+def choose_popularity(ti): # Função para escolher o caminho do DAG com base na popularidade do jogo mais jogado
     games = ti.xcom_pull(task_ids="get_games")
 
     top_game = games[0]
@@ -43,44 +44,46 @@ def choose_popularity(ti):
     return "normal_game"
 
 
-with DAG(
+with DAG( # Definição do DAG
     dag_id="steam_pipeline",
     start_date=datetime(2025, 1, 1),
     schedule="@daily",
     catchup=False,
 ) as dag:
 
-    get_games_task = PythonOperator(
+    get_games_task = PythonOperator( # Tarefa para obter os dados dos jogos mais jogados
         task_id="get_games",
         python_callable=get_games
     )
 
-    save_ranking_task = PythonOperator(
+    save_ranking_task = PythonOperator( # Tarefa para salvar o ranking dos jogos em um arquivo de texto
         task_id="save_ranking",
         python_callable=save_ranking
     )
 
-    branching = BranchPythonOperator(
+    branching = BranchPythonOperator( # Tarefa para escolher o caminho do DAG com base na popularidade do jogo mais jogado
         task_id="choose_popularity",
         python_callable=choose_popularity
     )
 
-    record_game = BashOperator(
+    record_game = BashOperator( # Tarefa para registrar a popularidade recorde do jogo mais jogado
         task_id="record_game",
         bash_command='echo "RECORD DE JOGADORES DETECTADO" >> /opt/airflow/logs/steam.log'
     )
 
-    normal_game = BashOperator(
+    normal_game = BashOperator( # Tarefa para registrar a popularidade normal do jogo mais jogado
         task_id="normal_game",
         bash_command='echo "POPULARIDADE NORMAL" >> /opt/airflow/logs/steam.log'
     )
 
-    finish = EmptyOperator(
+    finish = EmptyOperator( # tarefa para finalizar a Dag
         task_id="finish",
         trigger_rule="none_failed_min_one_success"
     )
 
-    get_games_task >> save_ranking_task >> branching
+    # Define as sequências das tarefas no DAG
+
+    get_games_task >> save_ranking_task >> branching 
 
     branching >> [record_game, normal_game]
 
